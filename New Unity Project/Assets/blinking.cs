22a8be0859deabad1aa0da33b540a561488c54b5
SungHo-PC\Sung Ho <kdf4750@naver.com>
2019-03-28 21:44:13 +0900

    FLASH BANG EFFECT SUCESS
    HAHAHAHAH

---
+++ New Unity Project/Assets/blinking.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class blinking : MonoBehaviour
+{
+
+    public CanvasGroup myCG;
+    private bool flash = false;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            MineHit();
+        }
+            if (flash)
+        {
+            myCG.alpha = myCG.alpha - Time.deltaTime * 1.5F;
+            if (myCG.alpha <= 0)
+            {
+                myCG.alpha = 0;
+                flash = false;
+            }
+        }
+    }
+
+    public void MineHit()
+    {
+        flash = true;
+        myCG.alpha = 1;
+    }
+}
