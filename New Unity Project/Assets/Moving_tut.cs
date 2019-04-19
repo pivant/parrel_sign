a4f295a3646dd1093ae3a3c2ae249bed0946e95e
SungHo-PC\Sung Ho <kdf4750@naver.com>
2019-04-19 23:06:32 +0900

    effect_and_tut
    tut

---
+++ New Unity Project/Assets/Moving_tut.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving_tut : MonoBehaviour
+{
+
+    public GameObject wasd_moving; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Invoke("delay_Activate", 2);
+    }
+
+    void delay_Activate()
+    {
+        wasd_moving.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
